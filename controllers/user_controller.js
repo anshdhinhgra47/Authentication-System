fbc4b20d4a3409768435cc98b1ac07dcef97c959
unknown <anshdhingra47@gmail.com>
2020-06-28 23:38:51 +0530

    Created and set up the User Sign Up Page

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,6 @@
+const User = require('../models/user');
+
+
 module.exports.user = (req, res) => {
 
     return res.render('user', {
@@ -24,7 +27,29 @@ module.exports.signIn = (req, res) => {
 //Get the Sign Up Data
 
 module.exports.create = function(req, res){
-    //TODO later
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){
+            console.log('Error in finding user in signing up');
+            return;
+        }
+
+        if(!user){
+            User.create(req.body, function(err, user){
+                if(err){
+                console.log('Error in creating user while signing up');
+                return;
+                }
+                
+                return res.redirect('/user/sign-in');
+            })
+        }else{
+            return res.redirect('back');
+        }
+    });
 }
 
 //Get the Sign In Data and create a session for the user
--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,12 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../controllers/user_controller');
-const userSignUp = require('../controllers/user_controller');
-const userSignIn = require('../controllers/user_controller');
+// const userSignUp = require('../controllers/user_controller');
+// const userSignIn = require('../controllers/user_controller');
 
 router.get('/welcome', userController.user);
-router.get('/sign-up', userSignUp.signUp);
-router.get('/sign-in', userSignIn.signIn);
+router.get('/sign-up', userController.signUp);
+router.get('/sign-in', userController.signIn);
 
-
+router.post('/create', userController.create);
 module.exports = router;
