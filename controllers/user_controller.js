810b30326e5209b5d939cac72da4ac49ed5cb41d
unknown <anshdhingra47@gmail.com>
2020-06-28 21:56:15 +0530

    Created the user sign-up and sign-in forms

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -2,5 +2,32 @@ module.exports.user = (req, res) => {
 
     return res.render('user', {
         title: "User page"
-    })
+    });
+};
+
+
+module.exports.signUp = (req, res) => {
+
+    return res.render('user_sign_up', {
+        title: "Sign Up"
+    });
+};
+
+module.exports.signIn = (req, res) => {
+
+    return res.render('user_sign_in', {
+        title: "Sign In"
+    });
+};
+
+
+//Get the Sign Up Data
+
+module.exports.create = function(req, res){
+    //TODO later
+}
+
+//Get the Sign In Data and create a session for the user
+module.exports.createSession = (req, res) => {
+    //TODO later
 }
