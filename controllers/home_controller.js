4abe0b947656c48fe3b443a44428d20175a2743c
unknown <anshdhingra47@gmail.com>
2020-06-28 22:28:26 +0530

    Created a Cookie Parser

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,6 @@
-module.exports.home = (req, res) => {
+module.exports.home = function(req, res){
+    console.log(req.cookies);
+    res.cookie('user_id', 45);
     return res.render('home', {
         title: "Home Page"
     })
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const port = 8000;
 const app = express();
 const path = require('path');
 const expressLayout = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+
+app.use(express.urlencoded());
+
+app.use(cookieParser());
 
 app.use(express.static('./assets'));
 
